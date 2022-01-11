5f1000277cfeabe92cc61d766bbf19d2e56eb1b1
arridhow <arridhow@gmail.com>
2022-01-11 13:59:14 +0700

    :rocket: init

--- README.md
+++ README.md
@@ -1,70 +1,48 @@
-# Getting Started with Create React App
+[![LinkedIn][linkedin-shield]][linkedin-url]
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<br />
+<p align="center">
+  <h1 style="padding-bottom:0" align="center">Search Dictionary</h1>
+  <p align="center">
+    A Web Dictionary
+    <br />
+  </p>
+</p>
 
-## Available Scripts
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h3 style="display: inline-block">Table of Contents</h3></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#live-demo">Live Demo</a></li>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+  </ol>
+</details>
 
-In the project directory, you can run:
+<!-- ABOUT THE PROJECT -->
 
-### `npm start`
+## About The Project
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+![Demo](/public/demo.png)
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+A web dictionary made with ReactJS and Tailwind CSS, deployed with Firebase.
+This is a college assignment project for the course `Teori Bahasa Automata` University of Riau.
 
-### `npm test`
+### Live Demo
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+[Search Dictionary](https://automata-search-engine.web.app)
 
-### `npm run build`
+### Built With
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- [ReactJS](https://reactjs.org/)
+- [TailwindCSS](https://tailwindcss.com/)
+- [Firebase](https://firebase.google.com/)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+[contributors-shield]: https://img.shields.io/github/contributors/arridhow/web-resume.svg?style=for-the-badge
+[contributors-url]: hhttps://github.com/arridhow/bootstrap-landing-portfolio/graphs/contributors
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/arridhopradana
--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
+import words from "./words.json";
 
 function App() {
+  // console.log(words);
+  const [filterData, setFilterData] = useState([]);
+  const [resultExample, setResultExample] = useState([]);
+  const [resultInfo, setResultInfo] = useState([]);
+  const [filterWord, setFilterWord] = useState("");
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+
+  const handleFilter = (e) => {
+    e.preventDefault();
+    setShow1(true);
+    const searchInput = e.target.value;
+    const newFilter = words.filter((word) => {
+      return word.toLowerCase().startsWith(searchInput);
+    });
+    setFilterData(newFilter);
+  };
+
+  // const handleWord = (word) => {
+  //   console.log(word);
+  // };
+
+  const handleWord = async (word) => {
+    await setFilterWord(word);
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const fetchResult = await fetch(url).then((data) => {
+      return data.json();
+    });
+
+    let tempResult;
+    tempResult = fetchResult[0].meanings.map((e) =>
+      e.definitions.map((a) => {
+        return a.example;
+      })
+    );
+    let resultExampleX = [...tempResult[0], ...tempResult[1]];
+    console.log(resultExampleX);
+    resultExampleX = resultExampleX.filter((e) => e !== undefined);
+    await setResultExample(resultExampleX);
+
+    tempResult = fetchResult[0].meanings.map((e) =>
+      e.definitions.map((a) => {
+        return a.definition;
+      })
+    );
+    const resultInfoX = [...tempResult[0], ...tempResult[1]];
+    await setResultInfo(
+      `The word ${word} has origin of ${fetchResult[0].origin} And have definitions which are ${resultInfoX}`
+    );
+    // console.log(resultInfoX);
+  };
+
+  const EmptyDiv = () => {
+    console.log("huhu");
+    return (
+      <div className="w-80 bg-white border border-gray-300 rounded-md mt-2 h-80" />
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex h-screen items-center justify-center flex-col">
+      <label className="relative block mb-4">
+        <span className="sr-only">Search</span>
+        <span className="absolute inset-y-0 left-0 flex items-center pl-4">
+          <svg
+            width="24"
+            height="24"
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.5 15.5L19 19"
+              stroke="#9ca3af"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M5 11C5 14.3137 7.68629 17 11 17C12.6597 17 14.1621 16.3261 15.2483 15.237C16.3308 14.1517 17 12.654 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11Z"
+              stroke="#9ca3af"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+        <input
+          className="placeholder:italic placeholder:text-gray-400 block w-80 border border-gray-300 rounded-md py-2 pl-12 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-lg"
+          placeholder="Ketik huruf..."
+          type="text"
+          name="search"
+          autoComplete="off"
+          onChange={handleFilter}
+        />
+      </label>
+      <div className="flex gap-6">
+        {show1 ? (
+          <div className="w-80 bg-white border border-gray-300 rounded-md mt-2">
+            <div className="flex flex-col max-h-80 overflow-x-hidden overflow-y-visible w-full">
+              {filterData.map((word, index) => {
+                return (
+                  <span
+                    className="py-2 pl-12 hover:bg-gray-300 cursor-pointer active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300"
+                    key={index}
+                    onClick={() => handleWord(word)}
+                  >
+                    {word}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <EmptyDiv />
+        )}
+        {show2 || resultExample.length !== 0 ? (
+          <div className="w-80 bg-white border border-gray-300 rounded-md mt-2 py-6 px-12 max-h-80 overflow-x-hidden overflow-y-visible">
+            {/* <span className="py-2 cursor-pointer">{filterWord}</span> */}
+            {resultExample.length !== 0 && (
+              <div>
+                <div className="">
+                  <ul className="list-disc pb-5">
+                    {resultExample.map((re, index) => {
+                      return re !== "" ? <li key={index}>{re}</li> : null;
+                    })}
+                  </ul>
+                  {/* <h2 className="text-lg font-semibold">Informations</h2>
+                  <p>{resultInfo}</p> */}
+                </div>
+              </div>
+            )}
+          </div>
+        ) : (
+          <EmptyDiv />
+        )}
+      </div>
     </div>
   );
 }
